57268d4bea890749bbcd735fca84ac96f89f8e3e
Kishore Mondi <kishoremondi2630@gmail.com>
2025-06-18 14:25:16 +0530

    test

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-pip install -r requirements.txt
-python -m spacy download en_core_web_sm
+pip install --prefer-binary -r requirements.txt
+python -m spacy download en_core_web_sm --direct
